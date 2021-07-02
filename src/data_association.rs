60319fa667c372625917fad46e997cf30c7e500f
Odin Aleksander Severinsen <odin.sev@gmail.com>
2021-07-02 23:59:59 +0200

    Start faster Murty

--- src/data_association.rs
+++ src/data_association.rs
@@ -1,9 +1,10 @@
-use ndarray::prelude::*;
-use std::collections::{VecDeque, BinaryHeap};
+use std::collections::VecDeque;
 use crate::argmax::argmax_iter;
-use crate::problem_solution_pair::{ProblemSolutionPair, Solution, Problem, InvalidSolutionError};
+use crate::problem_solution_pair::{Solution, Problem, InvalidSolutionError};
 
-use std::f64::NEG_INFINITY as neg_inf;
+
+pub mod murtys;
+
 
 pub mod auction_params {
     pub const EPS: f64 = 1e-3;
@@ -62,79 +63,6 @@ pub fn auction(problem: &Problem, eps: f64, max_iterations: usize) -> Result<Sol
     Solution::try_from_unvalidated_assignments(assigned_tracks)
 }
 
-pub fn murtys(original_problem: Problem, N: usize) -> Result<Vec<ProblemSolutionPair>, InvalidSolutionError> {
-    let (m, n) = original_problem.num_measurements_targets();
-    let original_solution = auction(&original_problem, auction_params::EPS, auction_params::MAX_ITERATIONS)?;
-    let problem_solution_pair = ProblemSolutionPair::new(original_solution, original_problem.clone())?; // TODO: Fix this later
-    let mut L = BinaryHeap::new();
-    L.push(problem_solution_pair);
-
-    let mut R = Vec::new();
-
-    while let Some(problem_solution_pair) = L.pop() {
-        // TODO: Be smarter here
-        R.push(problem_solution_pair.clone());
-
-        if R.len() == N {
-            break;
-        }
-        
-        let P = problem_solution_pair.problem();
-        let org_prob = P.clone();
-        let mut i = problem_solution_pair.solution().first();
-        
-        let mut locked_targets: Vec<usize> = Vec::new();
-        let mut item_idxs: Vec<_> = (0..P.num_measurements()).collect();
-
-        for t in 0..n {
-            P.make_association_impossible(i, 0);
-            if let Ok(solution) = auction(&P, auction_params::EPS, auction_params::MAX_ITERATIONS) {
-                let v = solution.into_vec();
-                let convert_v = get_indexed_vec(item_idxs.as_slice(), v.as_slice());
-                let Qs = Solution::concatenate_assignments(&[
-                &locked_targets,
-                &convert_v
-                ]);
-
-                let Qp = org_prob.clone();
-
-                let org_i = item_idxs[i];
-                Qp.make_association_impossible(org_i, t);
-
-                if let Ok(pair) = ProblemSolutionPair::new(Qs, Qp) {
-                    // Here we should make sure it does not exist already?
-                    if L.iter().all(|p| p != &pair) {
-                        L.push(pair);
-                    }
-                }
-            }
-
-            locked_targets.push(item_idxs[i]);
-            item_idxs.remove(i);
-            if P.num_targets() > 1{
-                P.delete_assignment(i);
-            } else {
-                break;
-            }
-            // if P.is_empty() {
-            //     break;
-            // }
-            
-            i = item_idxs.iter().position(|&i| i == problem_solution_pair.solution().assigned_measurement(t+1).unwrap()).expect("Cant find index??");
-        }
-    }
-
-    Ok(R)
-}
-
-
-fn get_indexed_vec<T: Copy>(v: &[T], idx: &[usize]) -> Vec<T> {
-    let mut index_vec = Vec::with_capacity(idx.len());
-    for &i in idx {
-        index_vec.push(v[i]);
-    }
-    index_vec
-}
 
 
 #[cfg(test)]
